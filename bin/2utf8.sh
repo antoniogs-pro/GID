fddc210e3e3d85e9d82a98fd30c8609c0ad543a9
antoniogs2015 <antoniogs2015@users.noreply.github.com>
2018-02-16 01:30:15 +0100

    Add files via upload

---
+++ bin/2utf8.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+########################################################################
+# Autor: Antonio González
+# Proposito del script: 
+# Fichero para pre-procesar y convertir ficheros de texto y CSV,  a UTF-8
+#
+# Última actualización: 2017/06/28
+## Ultimo cambio realizado: Añandir cabecera, eliminando mensajes por consola y pasarlos a fichero de LOG
+#
+# Como usarlo
+# echo "Como usarlo:  "$0  fichero_entrada"
+########################################################################
+#
+#######################################
+#Variables de entorno impresindibles  #
+#######################################
+#
+#Variables de entorno iniciales
+RUTA=${SICSCRIPTS=/home/sic/SIC/script_SIC}
+export FUNPROC=$SICSCRIPTS/FUNCYPROC
+
+#Variables de entorno generales
+if [ -f  $FUNPROC/entorno.sh ]; then . $FUNPROC/entorno.sh; fi
+## Agregado para perfilar mi entorno
+if [ -f  $FUNPROC/ordenes.sh ]; then . $FUNPROC/ordenes.sh; fi  
+# Incluir Funciones generales para perfilar mis SCRIPTS 
+if [ -f  $FUNPROC/funciones.sh ]; then . $FUNPROC/funciones.sh; fi 
+#Entorno específico
+LOGFILE=$LOGDIR/2utf8.log
+FICHEROENTRADA="$1"
+#
+##############################
+# COMPROBACIONES DE CONTROL  #
+##############################
+echo Ejecutando $0 sobre $1 >> $LOGFILE
+FICHEROENTRADA=$1
+if [ "$1" == "" ]
+then
+cat << :ayuda
+	 ATENCIÓN: 
+	 Debe especificar como argumento el nombre del FICHEROENTRADA TIPO TEXTO (CSV)  a PROCESAR Y COVERTIR A UTF-8.
+:ayuda
+ exit 1
+fi
+if [ ! -f $FICHEROENTRADA ]
+  then
+     cat << :ayuda
+	 ATENCIÓN: 
+	 No existe el ficheo $FICHEROENTRADA	
+:ayuda
+  exit 1
+fi
+################
+#   Proceso    #
+################
+file ${FICHEROENTRADA} | grep "UTF-8"  >> $LOGFILE
+if [ $? -gt 0 ] ; then
+#  echo ya tiene codificación UTF-8 >> $LOGFILE
+#  else
+#Convertir a UTF-8 sin BOM
+  # echo  ${FICHEROENTRADA} convertido a UTF-8 
+   vi ${FICHEROENTRADA} '+set fenc=utf-8' '+x'
+fi
+# 2º Ver si tiene BOM ?
+file $FICHEROENTRADA | grep "BOM"   >> $LOGFILE
+if [ $? == 0 ]; then
+  echo tiene BOM detectado por file, quitando  BOM  >> $LOGFILE
+  sed -i 's/^\xEF\xBB\xBF//' ${FICHEROENTRADA}
+fi
+# FIN
