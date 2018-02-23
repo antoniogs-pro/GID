ebcb47fd631d7b1fb3ccaec2094afb535bf42b9e
Antonio González <anglez@us.es>
2018-02-23 02:15:46 +0000

    Actualizar eliminar ficheros inutiles

--- script_SIC/GID-lanzar_win7.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash 
-/usr/bin/rdesktop -T "windows7_idm" -a 16 -k es -u grupocorreo -p -   -x lan -g 1024x750 192.168.3.103  
-#/usr/bin/rdesktop -T "windows7_idm" -a 16 -k es -u grupocorreo -p -   -x lan -g 850x750 192.168.3.103 
-
